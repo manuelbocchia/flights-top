d5854da662443561745d9012dc489ed22697f7b5
Manuel Bocchia <zerounodos@gmail.com>
2023-01-20 17:33:05 -0300

    Added top20 airports to the seed list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ 
+airports = [{name: "Hartsfield–Jackson Atlanta International Airport", code: "ATL", stateCode: "Atlanta, Georgia", countryCode: "US", countryName: "USA"},
+    {name: "Dallas Fort Worth International Airport", code: "DFW", stateCode: "Dallas-Fort Worth, Texas", countryCode: "", countryName: "USA"},
+    {name: "Denver International Airport", code: "DEN", stateCode: "Denver, Colorado", countryCode: "US", countryName: "USA"},
+    {name: "O'Hare International Airport", code: "ORD", stateCode: "Chicago, Illinois", countryCode: "US", countryName: "USA"},
+    {name: "Los Angeles International Airport", code: "LAX", stateCode: "Los Angeles, California", countryCode: "Los Angeles, California", countryName: "USA"},
+    {name: "Charlotte Douglas International Airport", code: "CLT", stateCode: "Charlotte, North Carolina", countryCode: "Charlotte, North Carolina", countryName: "USA"},
+    {name: "Orlando International Airport", code: "MCO", stateCode: "Orlando, Florida", countryCode: "US", countryName: "USA"},
+    {name: "Guangzhou Baiyun International Airport", code: "CAN", stateCode: "Baiyun-Huadu, Guangzhou, Guangdong", countryCode: "CH", countryName: "China"},
+    {name: "Chengdu Shuangliu International Airport", code: "CTU", stateCode: "Shuangliu-Wuhou, Chengdu, Sichuan", countryCode: "CH", countryName: "China"},
+    {name: "Harry Reid International Airport", code: "LAS", stateCode: "Las Vegas, Nevada", countryCode: "US", countryName: "USA"},
+    {name: "Phoenix Sky Harbor International Airport", code: "PHX", stateCode: "Phoenix, Arizona", countryCode: "US", countryName: "USA"},
+    {name: "Miami International Airport", code: "MIA", stateCode: "Miami-Dade County, Florida", countryCode: "US", countryName: "USA"},
+    {name: "Indira Gandhi International Airport", code: "DEL", stateCode: "Delhi", countryCode: "IN", countryName: "India"},
+    {name: "Istanbul Airport", code: "IST", stateCode: "Arnavutköy, Istanbul", countryCode: "TK", countryName: "Turkey"},
+    {name: "Shenzhen Bao'an International Airport", code: "SZX", stateCode: "Bao'an, Shenzhen, Guangdong", countryCode: "CH", countryName: "China"},
+    {name: "Seattle–Tacoma International Airport", code: "SEA", stateCode: "SeaTac, Washington", countryCode: "", countryName: "USA"},
+    {name: "Mexico City International Airport", code: "MEX", stateCode: "Venustiano Carranza, Mexico City", countryCode: "MX", countryName: "Mexico"},
+    {name: "China Chongqing Jiangbei International Airport", code: "CKG", stateCode: "Yubei, Chongqing", countryCode: "CH", countryName: "China"},
+    {name: "Shanghai Hongqiao International Airport", code: "SHA", stateCode: "Changning-Minhang, Shanghai", countryCode: "CH", countryName: "China"},
+    {name: "Beijing Capital International Airport", code: "PEK", stateCode: "Chaoyang-Shunyi, Beijing", countryCode: "CH", countryName: "China"}]
