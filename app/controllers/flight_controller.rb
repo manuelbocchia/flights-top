a567cf656644ce1a7229ddacfed9ff1c5118f96b
Manuel Bocchia <zerounodos@gmail.com>
2023-01-25 12:25:55 -0300

    Fixed nicer view of search results.

--- app/controllers/flight_controller.rb
+++ app/controllers/flight_controller.rb
@@ -3,9 +3,9 @@ class FlightController < ApplicationController
     def index
         @departure_options = Flight.all.map{ |f| [ f.departure.name, f.departure.id ] }
         @arrival_options = Flight.all.map{ |f| [ f.arrival.name, f.arrival.id ] }
-        @date_options = Flight.all.map{ |f| [f.date, f.date]}
+        @date_options = Flight.all.map{ |f| [f.date.strftime("%d/%m/%Y"), f.date]}
         @flights = Flight.all
-        @search = Flight.find_by_sql ["SELECT * FROM flights WHERE arrival_id = ? AND departure_id = ?", params[:arrival], params[:departure]] 
+        @search = Flight.find_by_sql ["SELECT * FROM flights WHERE arrival_id = ? AND departure_id = ?", params[:arrival], params[:departure]]
     end
 
 end
